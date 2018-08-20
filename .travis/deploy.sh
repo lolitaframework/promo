b7c3c748c40dab3ed69b072d04822af75d9f90f0
gcofficial <gurievcreative@gmail.com>
2018-08-20 19:31:38 +0300

    travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,6 +8,11 @@ ssh-add .travis/deploy # Add the private key to SSH
 echo -e "Host $IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-keyscan $IP >> ~/.ssh/known_hosts
 
+# push changes to the deployment environment
+git config --global push.default matching
+git remote add deploy ssh://deploy@$IP$DEPLOY_DIR
+git push deploy dev
+
 # build frontend after deploying
 ssh deploy@$IP <<EOF
   cd $DEPLOY_DIR
